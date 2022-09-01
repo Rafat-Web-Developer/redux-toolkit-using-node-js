445fc3e114e52cd42ad7f7c2cb8157dc71bb5a9e
Rafat-Web-Developer <rafat.webdeveloper@gmail.com>
2022-09-01 18:13:08 +0600

    Create a dynamic Counter feature

--- rtk/index.js
+++ rtk/index.js
@@ -1,10 +1,17 @@
 const store = require("./app/store");
 const { counterActions } = require("./features/counter/counterSlice");
+const {
+  dynamicCounterActions,
+} = require("./features/dynamicCounter/dynamicCounterSlice");
 
 store.subscribe(() => {
   console.log(store.getState());
 });
 
-store.dispatch(counterActions.increment());
-store.dispatch(counterActions.increment());
-store.dispatch(counterActions.decrement());
+// store.dispatch(counterActions.increment());
+// store.dispatch(counterActions.increment());
+// store.dispatch(counterActions.decrement());
+
+store.dispatch(dynamicCounterActions.increment(5));
+store.dispatch(dynamicCounterActions.increment(5));
+store.dispatch(dynamicCounterActions.decrement(5));
