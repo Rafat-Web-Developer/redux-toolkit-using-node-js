0b84a51490249b207c71bec11b5bef83566fefe9
Rafat-Web-Developer <rafat.webdeveloper@gmail.com>
2022-09-02 06:37:46 +0600

    Using Redux thunk in react toolkit

--- rtk/index.js
+++ rtk/index.js
@@ -3,15 +3,15 @@ const { counterActions } = require("./features/counter/counterSlice");
 const {
   dynamicCounterActions,
 } = require("./features/dynamicCounter/dynamicCounterSlice");
+const { fetchPosts } = require("./features/post/postSlice");
 
+// initial state
+// console.log(`Initial State: ${JSON.stringify(store.getState())}`);
+
+// subscribe to state changes
 store.subscribe(() => {
-  console.log(store.getState());
+  // console.log(store.getState());
 });
 
-// store.dispatch(counterActions.increment());
-// store.dispatch(counterActions.increment());
-// store.dispatch(counterActions.decrement());
-
-store.dispatch(dynamicCounterActions.increment(5));
-store.dispatch(dynamicCounterActions.increment(5));
-store.dispatch(dynamicCounterActions.decrement(5));
+// disptach actions
+store.dispatch(fetchPosts());
